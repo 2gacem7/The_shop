739e6e0827ba65d91245643626dd2b9a2f66651f
2gacem7 <gacemcode@gmail.com>
2021-02-22 09:48:35 +0100

    #0005 [ADDED] The adminboard has been added with the product input and table !

--- server/app/routes/product.routes.js
+++ server/app/routes/product.routes.js
@@ -4,7 +4,7 @@ module.exports = app => {
     var router = require("express").Router();
   
     // Create a new Tutorial
-    router.post("/add", product.create);
+    router.post("/", product.create);
   
     // Retrieve all Tutorials
     router.get("/", product.findAll);
